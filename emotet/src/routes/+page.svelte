530303f2277b90cd5483186e26ee39855c9ae4a0
MohaSetif <bouroubamohamed004@gmail.com>
2023-10-07 22:24:53 +0100

    emotet

--- emotet/src/routes/+page.svelte
+++ emotet/src/routes/+page.svelte
@@ -1,10 +1,49 @@
 <script lang="ts">
 	import { sections } from '$lib/index';
+	import { onMount } from 'svelte';
+	import malware from '../img/icons8-malware-66.png'
 	import './../app.css';
+
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	onMount(()=>{
+		const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
+      randomString = (length: Number) => Array.from(Array(length)).map(randomChar).join("");
+
+const card = document.querySelector(".card") as HTMLElement,
+      letters = card.querySelector(".card-letters") as HTMLElement;
+
+const handleOnMove = (e: any) => {
+  const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+  letters.style.setProperty("--x", `${x}px`);
+  letters.style.setProperty("--y", `${y}px`);
+
+  letters.innerText = randomString(1500);    
+}
+
+card.onmousemove = e => handleOnMove(e);
+
+card.ontouchmove = (e) => handleOnMove(e.touches[0]);
+	})
+
 </script>
 
 <div class="main">
-	<h1>Emotet presentation</h1>
+	<div class="card-track">
+		<div class="card-wrapper">
+		  <div class="card">
+			<div class="card-title">
+				<h1>Emotet</h1>
+			   <img src={malware} alt="malware">
+			</div>
+			<div class="card-gradient"></div>
+			<div class="card-letters"></div>
+		  </div>
+		</div>
+	  </div>
 	<div class="links">
 		{#each sections as section}
 			<a href="/sections/{section}">{section}</a>
@@ -21,7 +60,8 @@
 		align-items: center;
 	}
 	.main h1 {
-		width: 25%;
+		position: absolute;
+		z-index: 5;
 	}
 	.links {
 		display: flex;
